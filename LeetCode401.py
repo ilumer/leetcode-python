8636b625a4e0bdece4e1f51edd1d29501314f6b6
ilumer <ilumer.qxq@gmail.com>
2018-02-04 20:55:58 +0800

    add LeetCode401

---
+++ LeetCode401.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+import itertools
+
+
+class Solution(object):
+    def readBinaryWatch(self, num):
+        """
+        :type num: int
+        :rtype: List[str]
+        """
+        list = []
+        for i in range(0, 12):
+            for x in range(0, 60):
+                if (bin(i)+bin(x)).count('1') == num:
+                    list.append("{}:".format(i)+str(x).zfill(2))
+        return list
